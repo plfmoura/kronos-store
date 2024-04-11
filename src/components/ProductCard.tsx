a4862ff54f8d693a3fbb8cd83690e0c8bfee7bc4
plfmoura <plfmoura96@gmail.com>
2024-04-11 09:27:42 -0300

    feat: add product card

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+
+type ProductCardProps = {
+    id: number;
+    image: string;
+    name: string;
+    price: number;
+    stock: number;
+    onPress: () => void;
+};
+
+export default function ProductCard({
+    id,
+    image,
+    name,
+    price,
+    stock,
+    onPress,
+}: ProductCardProps) {
+    return (
+        <div
+            className="p-4 border border-gray-200 rounded-md flex flex-col items-center gap-2"
+        >
+            <Image
+                src={image}
+                alt={name}
+                width={300}
+                height={300}
+                className="w-full h-60 object-cover rounded-md"
+            />
+            <div className="w-full">
+                <h2 className="text-lg font-bold">{name}</h2>
+                <span>Preço: {price}</span>
+                <p>Em estoque: {stock}</p>
+            </div>
+            <button
+                onClick={onPress}
+                className="w-full bg-white text-slate-800 font-semibold px-4 py-2 rounded-md"
+            >
+                Adicionar ao carrinho
+            </button>
+        </div>
+    );
+};
