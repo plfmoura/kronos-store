fbcd2767df59f0a6550b7b068abeee3efd67ca60
WillianXG <wilian.mine789@gmail.com>
2024-04-14 14:15:24 -0300

    feat: Footer updates and icon addition

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -64,7 +64,6 @@ export default function Footer() {
 
                                 </svg>
                             </button>
-
                             <button
                                 className="text-gray-400 transition duration-100 hover:text-gray-500 active:text-gray-600"
                             >
@@ -76,7 +75,7 @@ export default function Footer() {
                                     fill="currentColor"
                                     xmlns="http://www.w3.org/2000/svg"
                                 >
-                                    <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+                                    <svg viewBox="0 0 192 192" xmlns="http://www.w3.org/2000/svg" fill="currentcolor"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path stroke="" stroke-linejoin="round" stroke-width="22" d="M22 57.265V142c0 5.523 4.477 10 10 10h24V95.056l40 30.278 40-30.278V152h24c5.523 0 10-4.477 10-10V57.265c0-13.233-15.15-20.746-25.684-12.736L96 81.265 47.684 44.53C37.15 36.519 22 44.032 22 57.265Z"></path></g></svg>
                                 </svg>
                             </button>
                         </div>
@@ -152,22 +151,19 @@ export default function Footer() {
                                     Contact
                                 </button>
                             </div>
-
-                            <div>
-                                <button className="text-gray-500 transition duration-100 hover:text-indigo-500 active:text-indigo-600">
-                                    Documentation
-                                </button>
-                            </div>
-
-                            <div>
-                                <button className="text-gray-500 transition duration-100 hover:text-indigo-500 active:text-indigo-600">
-                                    Chat
-                                </button>
-                            </div>
-
                             <div>
                                 <button className="text-gray-500 transition duration-100 hover:text-indigo-500 active:text-indigo-600">
                                     FAQ
+                                </button>
+                            </div>
+                            <div>
+                                <button className="text-gray-500 transition duration-100 hover:text-indigo-500 active:text-indigo-600">
+                                    Technical support
+                                </button>
+                            </div>
+                            <div>
+                                <button className="text-gray-500 transition duration-100 hover:text-indigo-500 active:text-indigo-600">
+                                    Feedback
                                 </button>
                             </div>
                         </nav>
