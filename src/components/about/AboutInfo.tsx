c3aabe6ebe7e46286484c1ce6ac436e9f09e76f5
WillianXG <wilian.mine789@gmail.com>
2024-04-15 14:32:24 -0300

    feat/update-about-page

--- src/components/about/AboutInfo.tsx
+++ src/components/about/AboutInfo.tsx
@@ -4,28 +4,25 @@ export default function AboutInfo() {
         <div className="w-full px-4 lg:w-1/2 xl:w-5/12">
             <div className="mt-10 lg:mt-0">
                 <span className="block mb-4 text-lg font-semibold text-primary">
-                    Lightning fast
+                    Unmatched punctuality
                 </span>
                 <h2 className="mb-5 text-3xl font-bold text-dark dark:text-white sm:text-[40px]/[48px]">
-                    Velocidade como você nunca viu
+                    Discover the measure of time in style
                 </h2>
                 <p className="mb-5 text-base text-body-color dark:text-dark-6">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                    Temporibus ducimus ab autem optio porro, eaque mollitia
-                    obcaecati corporis, rerum, ut reiciendis vero adipisci
-                    recusandae nostrum eligendi dicta asperiores magni culpa.
+                    Explore our collection of exceptional timepieces and discover the elegance that redefines the precision of time.
+                    At Kronos Store, every second is an opportunity to make a style statement.
                 </p>
                 <p className="mb-8 text-base text-body-color dark:text-dark-6">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet
-                    sapiente tenetur ad quia odio placeat voluptas ipsa numquam
-                    voluptates. Laborum quis, repudiandae doloremque assumenda
-                    eaque suscipit quia necessitatibus magnam deserunt!
+                    From technical innovation to the art of design, Kronos Store watches combine the best of watchmaking tradition with modernity.
+                    Find the perfect watch that suits your personality and lifestyle.
                 </p>
+
                 <a
                     href="#"
                     className="inline-flex items-center justify-center py-3 text-base font-medium text-center text-white border border-transparent rounded-md px-7 bg-primary hover:bg-opacity-90"
                 >
-                    Comece agora
+                    See our products
                 </a>
             </div>
         </div>
